0281a6b28c3b29d436ff4c88718c8affd0e49c53
dicellomichelangelo-debug <dicellomichelangelo@gmail.com>
2026-06-24 08:46:10 +0200

    Update CorsaDAO.java

--- src/main/java/acetomartina/DAO/CorsaDAO.java
+++ src/main/java/acetomartina/DAO/CorsaDAO.java
@@ -1,5 +1,29 @@
 package acetomartina.DAO;
 
-public class CorsaDAO {
+import acetomartina.entities.Corsa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class CorsaDao {
+    private final EntityManager entityManager;
+
+    //COSTRUTTORE
+    public CorsaDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // SALVO
+    public void save(Corsa corsa){
+        EntityTransaction transazione = this.entityManager.getTransaction();
+        try {
+            transazione.begin();
+            this.entityManager.persist(corsa);
+            transazione.commit();
+            System.out.println("La corsa con la tratta : "+ corsa.getTratta() + " è stata aggiunta al DATABASE");
+        } catch (Exception e) {
+            if (transazione.isActive()) transazione.rollback();
+            throw new RuntimeException("Errore durante il salvataggio della tratta : " + e.getMessage());
+        }
+    }
 }
